ba26d9d1b543f466670a53fb9ba2d8951c6d46da
CristianFlaviu <44320775+CristianFlaviu@users.noreply.github.com>
2021-05-10 01:39:13 +0300

    pay later

--- src/app/pages/favorite-products/favorite-products.component.ts
+++ src/app/pages/favorite-products/favorite-products.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from 'src/app/data_services/notification.service';
+import { ProductService } from 'src/app/data_services/products/product.service';
 
 @Component({
   selector: 'app-favorite-products',
   templateUrl: './favorite-products.component.html',
-  styleUrls: ['./favorite-products.component.scss']
+  styleUrls: ['./favorite-products.component.scss'],
 })
 export class FavoriteProductsComponent implements OnInit {
+  constructor(
+    private productService: ProductService,
+    private notificationService: NotificationService
+  ) {}
 
-  constructor() { }
+  public products: any[] = [];
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.productService.getProductFavorite().then((data) => {
+      this.products = data.payload;
+    });
   }
 
+  public async deleteProductShoppingCart(item: any) {
+    this.products = this.products.filter((x) => x !== item);
+    await this.productService.deleteProductFavorite(item?.product?.barcode);
+
+    this.notificationService.updateStats();
+  }
 }
